a6f63ac31df8716f494938dbedcdf03f293a23cd
Vertikar <vertigok@gmail.com>
2014-01-21 19:37:24 +1100

    Update cex.py
    add help

--- cex.py
+++ cex.py
@@ -6,6 +6,7 @@ import json
 
 @commands('cex')
 @example('.cex')
+@willie.module.commands('cex')
 def cex(bot, trigger):
     hdr = {'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.11 (KHTML, like Gecko) Chrome/23.0.1271.64 Safari/537.11',
        'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8',
